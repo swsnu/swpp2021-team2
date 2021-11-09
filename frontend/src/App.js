4f3b35bf5003d91522e30d8a912a1e6df504e7a3
ellenkim <kim.ellen9@gmail.com>
2021-11-10 00:04:53 +0900

    add search bar and states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar.js';
+import StateData from './states.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+      <SearchBar placeholder="Enter a state in the US..." data={StateData}/>
+    </div >
   );
 }
 
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+
+function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+  return (
+    <div className="search">
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        <div className="searchIcon">
+          {filteredData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput} />
+          )}
+        </div>
+      </div>
+      {filteredData.length != 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+                <div className="dataItem"> {value} </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchBar;
