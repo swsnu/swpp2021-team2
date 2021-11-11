21b3fce7b63c18c3a0f7daa69561abc37246fb4f
ellenkim <kim.ellen9@gmail.com>
2021-11-11 14:50:19 +0900

    add generate button

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,36 +1,59 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
 import SelectSearch from "react-select-search";
-import statesData from "../states.json"
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import statesData from "../states.json";
 import Fuse from "fuse.js";
 
 function SearchBar() {
   const [value, setValue] = useState("");
-  var states = Object.assign(statesData.map((state) => ({name: state, value: state})))
+  var states = Object.assign(
+    statesData.map((state) => ({ name: state, value: state }))
+  );
 
   return (
-    <SelectSearch
-      options={states}
-      value={value}
-      onChange={setValue}
-      search
-      filterOptions={(options) => {
-        const fuse = new Fuse(options, {
-          keys: ["name", "value", "items.name"],
-          threshold: 0.3
-        });
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <SelectSearch
+            options={states}
+            value={value}
+            onChange={setValue}
+            search
+            filterOptions={(options) => {
+              const fuse = new Fuse(options, {
+                keys: ["name", "value", "items.name"],
+                threshold: 0.3,
+              });
 
-        return (value) => {
-          if (!value.length) {
-            return options;
-          }
+              return (value) => {
+                if (!value.length) {
+                  return options;
+                }
 
-          return fuse.search(value);
-        };
-      }}
-      emptyMessage="Not found"
-      placeholder="Enter a state..."
-    />
+                return fuse.search(value);
+              };
+            }}
+            emptyMessage="Not found"
+            placeholder="Enter a state..."
+          />{" "}
+        </Grid>
+        <Grid item xs={4}>
+          <Button
+            className="generate-button"
+            variant="contained"
+            size="small"
+            style={{marginLeft:"17px", marginTop:"17px"}}
+            onClick={() => {
+              console.log(value);
+            }}
+          >
+            Generate
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
