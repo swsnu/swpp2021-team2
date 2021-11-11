599e58c5885531925cdb449d8b66e9ed85c114b2
ellenkim <kim.ellen9@gmail.com>
2021-11-11 16:34:02 +0900

    generate itinerary function

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -12,6 +12,54 @@ function SearchBar() {
     statesData.map((state) => ({ name: state, value: state }))
   );
 
+  const options = {
+    headers: {
+      Authorization:
+        "5ae2e3f221c38a28845f05b6e976bb53f7ead9f5b8d57fb053738023",
+    },
+  };
+
+  async function fetchCoordinates(localUrl) {
+    const response = await fetch(localUrl, options)
+      .then((res) => res.json())
+      .then((data) => data);
+    const placeUrl = `https://api.opentripmap.com/0.1/en/places/radius?radius=1000&lon=${response.lon}&lat=${response.lat}&limit=5&format=json&apikey=5ae2e3f221c38a28845f05b6e976bb53f7ead9f5b8d57fb053738023`;
+
+    const response2 = await fetch(placeUrl, options)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+  function generateItin(loc) {
+    const localUrl = `https://api.opentripmap.com/0.1/en/places/geoname?name=${loc}&apikey=5ae2e3f221c38a28845f05b6e976bb53f7ead9f5b8d57fb053738023`;
+    fetchCoordinates(localUrl);
+  }
+
+  // const response3 = await fetch(placeUrl, options).then(function (data) {
+  //   document.getElementById.innerHTML = `<p>5 objects with description in a 1km radius</p>`;
+  //   loadList();
+  // });
+
+  // function loadList() {
+  //   fetch(placeUrl, options).then(function (data) {
+  //     let list = document.getElementById("list");
+  //     list.innerHTML = "";
+  //     data.forEach((item) => list.appendChild(createListItem(item)));
+  //   });
+  // }
+
+  // function createListItem(item) {
+  //   let a = document.createElement("a");
+  //   a.className = "list-group-item list-group-item-action";
+  //   a.setAttribute("data-id", item.xid);
+  //   a.innerHTML = `<h5 class="list-group-item-heading">${item.name}</h5>
+  //                 <p class="list-group-item-text">${item.kinds}</p>`;
+  //   return a;
+  // }
+
+  // firstLoad();
+  // reportWebVitals();
+
   return (
     <div>
       <Grid container spacing={2}>
@@ -44,9 +92,9 @@ function SearchBar() {
             className="generate-button"
             variant="contained"
             size="small"
-            style={{marginLeft:"17px", marginTop:"17px"}}
+            style={{ marginLeft: "17px", marginTop: "17px" }}
             onClick={() => {
-              console.log(location);
+              generateItin(location);
             }}
           >
             Generate
